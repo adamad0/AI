bf0208b1033aa736937db959c69f73415f845cb5
adamad0 <89034465+adamad0@users.noreply.github.com>
2021-08-18 11:34:06 +0300

    Add files via upload

---
+++ locat - Kopya.py
@@ -0,0 +1,31 @@
+from geolite2 import geolite2
+
+
+
+
+
+def iplocation(ip):
+    reader = geolite2.reader()
+    location = reader.get(ip)
+
+
+    a=(location['city']['names']['en'])
+    b=(location['continent']['names']['en'])
+    c=(location['country']['names']['en'])
+    d=(location['location'])
+    e=(location['postal'])
+    f=(location['registered_country']['names']['en'])
+    g=(location['subdivisions'][0]['names']['en'])
+
+    print('''city: %s\ncontinent: %s\ncountry: %s\nlocation: %s\npostal: %s\nregistered_country: %s\nsubdivisions: %s\n'''
+     % (a,b,c,d,e,f,g))
+
+
+
+
+    
+
+
+ip = ""
+
+iplocation(ip)
