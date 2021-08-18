181998699dfe7d4e37f8484047f6a43072af4512
adamad0 <m.samikarakelleoglu@gmail.com>
2021-08-18 11:24:08 +0300

    Create Urlden ip bulma.py

---
+++ Urlden ip bulma.py
@@ -0,0 +1,13 @@
+import socket
+
+from ip2geotools.databases.noncommercial import DbIpCity
+
+url= input("Insert a URL:")
+IP=socket.gethostbyname(url)
+
+response=DbIpCity.get(IP,api_key="free")
+
+print("IP:",IP)
+print("City:",response.city)
+print("Region:",response.region)
+print("Country:",response.country)
